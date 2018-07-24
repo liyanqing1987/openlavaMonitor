bef233bece09370a00ed7f77ad378b4e12136294
liyanqing1987 <liyanqing1987@163.com>
2018-07-24 09:47:31 +0800

    update documents.

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ def read(fname):
 
 setup(
     name='openlavaMonitor',
-    version='1.0',
+    version='1.1',
     author='yanqing.li',
     author_email='liyanqing1987@163.com',
     url='https://github.com/liyanqing1987/openlavaMonitor/',
